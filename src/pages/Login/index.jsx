45260d0730e2b88be203294053e7336fc507346f
Yasenbaka <1260352143@qq.com>
2024-01-27 21:59:59 +0800

    2024012702

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./index.css";
+import axios from "axios";
+
 
 function Login() {
-    return (
-        <div className={"login-container"}>
-            <div className={"login-box"}>
-                <h2 className={"login-title"}>登入系统</h2>
-                <input className={"login-input-line"} type={"text"} id={"qq-userid"} placeholder={"请输入学号或秦小油工号"}/>
-                <input className={"login-input-line"} type={"password"} id={"group-userid"} placeholder={"请输入密码"}/>
-                <button id={"login-button"}>提交</button>
-            </div>
+    let [isCookieLogin, setCookieState] = useState(false);
+    const checkCookie = () => {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].split('=')[0].trim();
+            if (cookie === 'login') setCookieState(true);// Cookie存在
+        }
+    }
+    useEffect(() => {
+        checkCookie();
+    });
+    const loginButtonClicked = () => {
+        axios.get('http://localhost:8000/tologin')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('请求失败:', error);
+            });
+    }
+    const notLogin = <div className={"login-container"}>
+        <div className={"login-box"}>
+            <h2 className={"login-title"}>登入系统</h2>
+            <input className={"login-input-line"} type={"text"} id={"qq-userid"} placeholder={"请输入学号或秦小油工号"}/>
+            <input className={"login-input-line"} type={"password"} id={"group-userid"} placeholder={"请输入密码"}/>
+            <button onClick={loginButtonClicked} id={"login-button"}>提交</button>
         </div>
+    </div>
+    const logged = <div className={"login-container"}>
+        <h2 className={"login-title"}>你已经登入系统了！请返回主页！</h2>
+    </div>
+    return (
+        isCookieLogin? logged: notLogin
     )
 }
 
--- src/pages/Management/index.jsx
+++ src/pages/Management/index.jsx
@@ -1,9 +1,22 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 function Management(){
+    const navigate = useNavigate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const checkCookie = () => {
+        const cookies = document.cookie.split(';');
+        let isCookieLogin = false;
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].split('=')[0].trim();
+            if (cookie === 'login') isCookieLogin = true;// Cookie存在，不进行跳转
+        }
+        if (!isCookieLogin) navigate('/login');// Cookie不存在，进行跳转
+    }
+    useEffect(() => {checkCookie();}, [checkCookie]);
     return (
         <div>
-            <h1>请在上方导航选择你的工作内容！</h1>
+            <h1>这里是后台管理页，请根据导航栏选择工作</h1>
             <Outlet/>
         </div>
     )
